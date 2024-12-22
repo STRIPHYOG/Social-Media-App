18c863c01fdff165cc877d2bb7452e6932399eb9
Sachidananda Tripathy <105988018+STRIPHYOG@users.noreply.github.com>
2024-12-22 14:02:12 +0530

    Delete classsed-graphql-mern-apollo-master/config.js

--- classsed-graphql-mern-apollo-master/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB: 'mongodb+srv://classsed:likun1234@cluster0.hxmfg.mongodb.net/classsed-graphql-mern-apollo-master?retryWrites=true&w=majority',
-  SECRET_KEY: 'some very secret key'
-
-};
